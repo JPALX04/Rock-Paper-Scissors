f7278489985bf2e703e683365d30d93d0665479c
JPALX04 <184169107+JPALX04@users.noreply.github.com>
2025-03-04 20:39:06 -0300

    refactor: move inline scripts to external JS file
    Move all inline JavaScript code to a new file `script.js`.
    Update HTML files to reference the external JS file using `<script src="script.js"></script>`.
    Remove inline `<script>` tags from HTML files.

--- script.js
+++ script.js
@@ -1,11 +1,114 @@
+let computerChoice;
+let humanChoice;
+let humanScore = 0;
+let computerScore = 0;
+
+//function to get the computer choice
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3) + 1;
+  computerChoice = Math.floor(Math.random() * 3) + 1;
+  switch (computerChoice) {
+    case 1:
+      return (computerChoice = "rock");
+
+      break;
+    case 2:
+      return (computerChoice = "paper");
+      break;
+    case 3:
+      return (computerChoice = "scissor");
+      break;
+  }
 }
 
+//function to get the human choice
 function getHumanChoice() {
-  choice = prompt(
+  humanChoice = prompt(
     "choose your weapon: Rock, paper or scissor",
     ""
   ).toLowerCase();
 
+  switch (humanChoice) {
+    case "rock":
+      return humanChoice;
+      break;
+    case "paper":
+      return humanChoice;
+      break;
+    case "scissor":
+      return humanChoice;
+      break;
+    default:
+      alert("Type one of the options!");
+      getHumanChoice();
+  }
 }
+
+//game round
+function playRound(humanChoice, computerChoice) {
+  computerChoice = getComputerChoice();
+  //  console.log(computerChoice); <== test if the logic was correct
+  humanChoice = getHumanChoice();
+
+  if (humanChoice === computerChoice) {
+    console.log("This is a draw");
+  } else if (humanChoice === "rock") {
+    switch (computerChoice) {
+      case "paper":
+        ++computerScore;
+        console.log("You lose! Paper beats Rock");
+
+        break;
+      case "scissor":
+        ++humanScore;
+        console.log("You won! Rock beats Scissor!");
+
+        break;
+    }
+  } else if (humanChoice === "paper") {
+    switch (computerChoice) {
+      case "rock":
+        ++humanScore;
+        console.log("You won! Paper beats Rock!");
+
+        break;
+      case "scissor":
+        ++computerScore;
+        console.log("You lose! Scissor beats Paper");
+    }
+  } else if (humanChoice === "scissor") {
+    switch (computerChoice) {
+      case "paper":
+        ++humanScore;
+        console.log("You won! Scissor beats Paper");
+
+        break;
+      case "rock":
+        ++computerScore;
+        console.log("You lose! Rock beats Scissor");
+    }
+  }
+  playGame();
+}
+
+//function to play 5 rounds
+
+function playGame() {
+  if (humanScore < 5 && computerScore < 5) {
+    console.log(`Your score is: ${humanScore}`);
+    console.log(`The Machine score is: ${computerScore}`);
+    playRound();
+  } else if (humanScore === 5) {
+    alert("You've beaten the machine!");
+    humanScore = 0;
+    computerScore = 0;
+    console.clear();
+    playGame();
+  } else if (computerScore === 5) {
+    alert("You lose! The machines will take control of the world");
+    humanScore = 0;
+    computerScore = 0;
+    console.clear();
+    playGame();
+  }
+}
+playGame();
