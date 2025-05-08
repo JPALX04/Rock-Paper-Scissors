e95e79a4f7fd018ad118852bde706901c03e6901
JPALX04 <184169107+JPALX04@users.noreply.github.com>
2025-05-08 17:18:47 -0300

    refactor: replace getHumanChoice with button event listeners
    Add Rock/Paper/Scissors buttons to index.html
    Remove getHumanChoice()
    Attach event listeners to buttons for human choice selection

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ let humanChoice;
 let humanScore = 0;
 let computerScore = 0;
 
+document.body.addEventListener("click", (e) => {
+  if (e.target.nodeName.toLowerCase() === "button") {
+    humanChoice = e.target.textContent.toLowerCase();
+    computerChoice = getComputerChoice();
+
+    playRound();
+  }
+});
+
 //function to get the computer choice
 function getComputerChoice() {
   computerChoice = Math.floor(Math.random() * 3) + 1;
   switch (computerChoice) {
     case 1:
       return (computerChoice = "rock");
-
       break;
     case 2:
       return (computerChoice = "paper");
@@ -20,34 +28,10 @@ function getComputerChoice() {
   }
 }
 
-//function to get the human choice
-function getHumanChoice() {
-  humanChoice = prompt(
-    "choose your weapon: Rock, paper or scissor",
-    ""
-  ).toLowerCase();
-
-  switch (humanChoice) {
-    case "rock":
-      return humanChoice;
-      break;
-    case "paper":
-      return humanChoice;
-      break;
-    case "scissor":
-      return humanChoice;
-      break;
-    default:
-      alert("Type one of the options!");
-      getHumanChoice();
-  }
-}
-
 //game round
-function playRound(humanChoice, computerChoice) {
-  computerChoice = getComputerChoice();
-  //  console.log(computerChoice); <== test if the logic was correct
-  humanChoice = getHumanChoice();
+function playRound() {
+  // console.log(computerChoice); //<== test if the logic was correct
+  // console.log(humanChoice);
 
   if (humanChoice === computerChoice) {
     console.log("This is a draw");
@@ -87,28 +71,48 @@ function playRound(humanChoice, computerChoice) {
         console.log("You lose! Rock beats Scissor");
     }
   }
-  playGame();
+  // playGame();
 }
+
+//function to get the human choice
+// function getHumanChoice() {
+//   humanChoice = e.target;
+
+//   switch (humanChoice) {
+//     case "rock":
+//       return humanChoice;
+//       break;
+//     case "paper":
+//       return humanChoice;
+//       break;
+//     case "scissor":
+//       return humanChoice;
+//       break;
+//     default:
+//       alert("Type one of the options!");
+//       getHumanChoice();
+//   }
+// }
 
 //function to play 5 rounds
 
-function playGame() {
-  if (humanScore < 5 && computerScore < 5) {
-    console.log(`Your score is: ${humanScore}`);
-    console.log(`The Machine score is: ${computerScore}`);
-    playRound();
-  } else if (humanScore === 5) {
-    alert("You've beaten the machine!");
-    humanScore = 0;
-    computerScore = 0;
-    console.clear();
-    playGame();
-  } else if (computerScore === 5) {
-    alert("You lose! The machines will take control of the world");
-    humanScore = 0;
-    computerScore = 0;
-    console.clear();
-    playGame();
-  }
-}
-playGame();
+// function playGame() {
+//   if (humanScore < 5 && computerScore < 5) {
+//     console.log(`Your score is: ${humanScore}`);
+//     console.log(`The Machine score is: ${computerScore}`);
+//     playRound();
+//   } else if (humanScore === 5) {
+//     alert("You've beaten the machine!");
+//     humanScore = 0;
+//     computerScore = 0;
+//     console.clear();
+//     playGame();
+//   } else if (computerScore === 5) {
+//     alert("You lose! The machines will take control of the world");
+//     humanScore = 0;
+//     computerScore = 0;
+//     console.clear();
+//     playGame();
+//   }
+// }
+// playGame();
